f00be74d5703636c849f0aaae48983f9e047b7cd
Kyle <rrblack701@gmail.com>
2024-10-07 16:29:46 +0900

    Blog

---
+++ Blog/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/index.html')
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html")
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+        return render_template("post.html", post = requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
